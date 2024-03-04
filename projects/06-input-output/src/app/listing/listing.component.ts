cc22d443f86c9106890c11253ac9c51500272cfa
Lynn Locksley <lynnannlocksley@gmail.com>
2024-03-04 12:44:14 +0530

    Control flow and Input Output

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,45 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `
+    <!-- This article element represents and entire listing -->
+    <article class="listing">
+      <div class="image-parent">
+        <img class="product-image" src="https://placehold.co/100x100" />
+      </div>
+      <section class="details">
+        <p class="title">{{car.model}} {{car.make}}</p>
+        <hr />
+        <p class="detail">
+        <span>Year</span>
+        <span>{{car.year}}</span>
+        </p>
+        <div class="detail">
+          <span>Transmission</span>
+          <span>{{car.transmission}}</span>
+        </div>
+        <p class="detail">
+        <span>Mileage</span>
+        <span>{{car.miles}}</span>
+        </p>
+        <p class="detail">
+        <span>Price</span>
+        <span>{{car.price}}</span>
+        </p>
+      </section>
+      <button (click)="handleSavedCar()">Add To</button>
+    </article>
+    `,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({
+    required:true //required component
+  }) car!: Car; //non null assertion operator tell angular to chill if it is undefined
+
+  @Output() savedCar = new EventEmitter<Car>();
+
+  handleSavedCar(){
+    this.savedCar.emit(this.car);
+  }
+}
